2fe738ba8d3e0382fdba7771f90bd956e0b64f21
Janette <janetterp2@gmail.com>
2026-03-16 00:07:47 +0100

    ex 0 - 6

---
+++ ft_garden_analytics.py
@@ -0,0 +1,38 @@
+class Plant:
+    def __init__(self, name: str, height: int, age: int):
+        self.name = name  # Atributo de instancia
+        self.height = height  # Atributo de instancia
+        self.age = age
+
+    def get_info(self) -> str:
+        return f"{self.name}: {self.height}cm"
+
+    def growth(self):
+        self.height += 1
+
+    def aged(self):
+        self.age += 1
+
+
+class FloweringPlant(Plant):
+    def __init__(self, name: str, height: int, age: int, color: str):
+        super().__init__(name, height, age)
+        self.color = color
+
+    def bloom(self) -> None:
+        print(f"{self.name} is blooming beautifully!")
+
+    def get_info(self) -> str:
+        info_plant = super().get_info()
+        return f"{info_plant}, {self.color} flowers (blooming)"
+
+
+class PrizeFlower(FloweringPlant):
+    def __init__(self, name: str, height: int, age: int, color: str,
+                 points: int):
+        super().__init__(name, height, age, color)
+        self.points = points
+
+    def get_info(self) -> str:
+        info_flower = super().get_info()
+        return (f"{info_flower}, Prize points: {self.points}")
